9f16723c79a6594c1d2b7dd27095cdac0376b309
Jashan Chahal <chahaljashan123@gmail.com>
2021-01-19 15:27:14 +0530

    Refactored code(moved text sending capability in a function)

--- whatsappBot.js
+++ whatsappBot.js
@@ -1,4 +1,18 @@
 const puppeteer = require("puppeteer");
+const TEXT_INPUT_FIELD = "#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text"
+const SMILEY = "#main > footer > div._3SvgF._1mHgA.copyable-area > div._3qpzV.rN1v9 > div.nw8RZ._1isZ2 > button._37evB._16P6V._2gB8p.hLiSc._3guyl > span"
+const STICKER_ICON = " div._3qpzV.rN1v9 > div.nw8RZ._1isZ2._3c42S > button._37evB._16P6V._3x5p4._3guyl > span";
+
+async function sendText(messageArr, destination) {
+    { inputField, page } = destination;
+    if (!!messageArr === false || !!inputField === false) {
+        return;
+    }
+    for (let i = 0; i < messageArr.length; i++) {
+        await inputField.type(messageArr[i]);
+        await page.keyboard.press("Enter");
+    }
+}
 
 async function whatsappBot(message) {
     const browser = await puppeteer.launch({ headless: false });
@@ -13,43 +27,44 @@ async function whatsappBot(message) {
     let repeat = !!message.repeat ? message.repeat : 1;
 
     if (!!message.stickers) {
-        const smily = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div._3qpzV.rN1v9 > div.nw8RZ._1isZ2 > button._37evB._16P6V._2gB8p.hLiSc._3guyl > span");
-        await smily.click();
+        const smiley = await page.$(SMILEY);
+        await smiley.click();
 
-        const stickerSpan = " div._3qpzV.rN1v9 > div.nw8RZ._1isZ2._3c42S > button._37evB._16P6V._3x5p4._3guyl > span"
-        await page.waitForSelector(stickerSpan);
-        const stickerButton = await page.$(stickerSpan);
+        const stickerIcon = STICKER_ICON;
+        await page.waitForSelector(stickerIcon);
+        const stickerButton = await page.$(stickerIcon);
         await stickerButton.click();
 
         let k = 1;
         for (let i = 0; i < repeat; i++) {
-            const st = `#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(${k}) > div > img`;
-            await page.waitForSelector(st);
-            const dance = await page.$(st);
+            const sticker = `#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(${k}) > div > img`;
+            await page.waitForSelector(sticker);
+            const dance = await page.$(sticker);
             await dance.click();
             k = (k) % 3 + 1;
         }
     } else if (!!message.text) {
-        const imp = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text")
-        let text = !!message.text ? message.text : [];
+        const inputField = await page.$(TEXT_INPUT_FIELD);
+        const destination = {
+            page,
+            inputField
+        }
         for (let i = 0; i < repeat; i++) {
-            for (let j = 0; j < text.length; j++) {
-                await imp.type(text[j]);
-                await page.keyboard.press("Enter");
-            }
+            await sendText(message.text, destination);
         }
 
     }
 
 
 }
-
+let recepient = process.argv[2];
+let isGroup = !!process.argv[3] ? (process.argv[3].toLowerCase() == "group") : false;
 const message = {
-    recepient: "Chirag NITJ CSE",
-    isGroup: true,
+    recepient,
+    isGroup,
     stickers: false,
-    text: ["hahaha", "hey", "Hi", "whatsup", "how are you"],
-    repeat: 20
+    text: ["hello", "Hi", "whatsup", "how are you"],
+    repeat: 200
 }
 try {
     whatsappBot(message);
