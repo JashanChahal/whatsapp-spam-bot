66d2ba703f58ff88ef87878d3a54a0b78665f46a
Jashan Chahal <chahaljashan123@gmail.com>
2021-01-19 13:04:39 +0530

    refactored code and added group spamming

--- whatsappBot.js
+++ whatsappBot.js
@@ -1,12 +1,13 @@
 const puppeteer = require("puppeteer");
 
-async function scrape(url, title) {
+async function whatsappBot(message) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto("https://web.whatsapp.com");
 
-    await page.waitForSelector(`span [title=\'${title}\']`);
-    const target = await page.$(`span [title=\'${title}\']`);
+    const element = !!message.isGroup ? "div" : "span";
+    await page.waitForSelector(`${element} [title=\'${message.recepient}\']`);
+    const target = await page.$(`${element} [title=\'${message.recepient}\']`);
     await target.click();
 
     const smily = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div._3qpzV.rN1v9 > div.nw8RZ._1isZ2 > button._37evB._16P6V._2gB8p.hLiSc._3guyl > span");
@@ -17,12 +18,15 @@ async function scrape(url, title) {
     const stickerButton = await page.$(stickerSpan);
     await stickerButton.click();
 
-    const danceSticker = "#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(1) > div > img"
-    await page.waitForSelector(danceSticker);
-    const dance = await page.$("#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(1) > div > img")
+    let k = 1;
     for (let i = 0; i < 100; i++) {
+        const st = `#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(${k}) > div > img`;
+        await page.waitForSelector(st);
+        const dance = await page.$(st);
         await dance.click();
+        k = (k) % 3 + 1;
     }
+
     // const imp = await page  .$("#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text")
     // for(let i=0;i<4000;i++){
     //     await imp.type('Chirag lodu');
@@ -31,6 +35,13 @@ async function scrape(url, title) {
     // }
 
 }
-{/* 
- */}
-scrape("https://web.whatsapp.com", "Shubam thind")
+
+const message = {
+    recepient: "Chirag NITJ CSE",
+    isGroup: true,
+}
+try {
+    whatsappBot(message);
+} catch (error) {
+    console.log(error);
+}
