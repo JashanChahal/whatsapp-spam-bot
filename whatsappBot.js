f1e56ebe7faab3667262538d27d31623dad3ea2a
Jashan Chahal <chahaljashan123@gmail.com>
2021-01-19 13:39:14 +0530

    added text functionality

--- whatsappBot.js
+++ whatsappBot.js
@@ -10,35 +10,46 @@ async function whatsappBot(message) {
     const target = await page.$(`${element} [title=\'${message.recepient}\']`);
     await target.click();
 
-    const smily = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div._3qpzV.rN1v9 > div.nw8RZ._1isZ2 > button._37evB._16P6V._2gB8p.hLiSc._3guyl > span");
-    await smily.click();
+    let repeat = !!message.repeat ? message.repeat : 1;
 
-    const stickerSpan = " div._3qpzV.rN1v9 > div.nw8RZ._1isZ2._3c42S > button._37evB._16P6V._3x5p4._3guyl > span"
-    await page.waitForSelector(stickerSpan);
-    const stickerButton = await page.$(stickerSpan);
-    await stickerButton.click();
+    if (!!message.stickers) {
+        const smily = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div._3qpzV.rN1v9 > div.nw8RZ._1isZ2 > button._37evB._16P6V._2gB8p.hLiSc._3guyl > span");
+        await smily.click();
 
-    let k = 1;
-    for (let i = 0; i < 100; i++) {
-        const st = `#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(${k}) > div > img`;
-        await page.waitForSelector(st);
-        const dance = await page.$(st);
-        await dance.click();
-        k = (k) % 3 + 1;
+        const stickerSpan = " div._3qpzV.rN1v9 > div.nw8RZ._1isZ2._3c42S > button._37evB._16P6V._3x5p4._3guyl > span"
+        await page.waitForSelector(stickerSpan);
+        const stickerButton = await page.$(stickerSpan);
+        await stickerButton.click();
+
+        let k = 1;
+        for (let i = 0; i < repeat; i++) {
+            const st = `#main > footer > div._3pk0T > div > div:nth-child(3) > div._3Xjbn > div.s-vzs > div._3Z2oJ > div > div > div > div:nth-child(${k}) > div > img`;
+            await page.waitForSelector(st);
+            const dance = await page.$(st);
+            await dance.click();
+            k = (k) % 3 + 1;
+        }
+    } else if (!!message.text) {
+        const imp = await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text")
+        let text = !!message.text ? message.text : [];
+        for (let i = 0; i < repeat; i++) {
+            for (let j = 0; j < text.length; j++) {
+                await imp.type(text[j]);
+                await page.keyboard.press("Enter");
+            }
+        }
+
     }
 
-    // const imp = await page  .$("#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text")
-    // for(let i=0;i<4000;i++){
-    //     await imp.type('Chirag lodu');
-    //     await page.keyboard.press("Enter");
-
-    // }
 
 }
 
 const message = {
     recepient: "Chirag NITJ CSE",
     isGroup: true,
+    stickers: false,
+    text: ["hahaha", "hey", "Hi", "whatsup", "how are you"],
+    repeat: 20
 }
 try {
     whatsappBot(message);
